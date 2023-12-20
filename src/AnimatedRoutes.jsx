2e2fdfbc9725abd6739e332e566d70bde364e3f1
vlas237 <83783001+vlas237@users.noreply.github.com>
2023-12-19 21:18:19 -0300

    dark theme arrow colors

--- src/AnimatedRoutes.jsx
+++ src/AnimatedRoutes.jsx
@@ -21,7 +21,8 @@ function AnimatedRoutes() {
   };
 
   const navigateToPrev = () => {
-    const prevRouteIndex = (currentRouteIndex - 1 + routes.length) % routes.length;
+    const prevRouteIndex =
+      (currentRouteIndex - 1 + routes.length) % routes.length;
     const prevRoute = routes[prevRouteIndex];
     navigate(prevRoute);
   };
@@ -52,18 +53,18 @@ function AnimatedRoutes() {
       </div>
       <div className="hidden lg:block">
         <div className="fixed left-4 z-10 flex items-center">
-          <button 
+          <button
             onClick={navigateToPrev}
-            className="text-gray-600 dark:text-gray-200 font-bold px-10 h-screen text-4xl transition duration-300 ease-in-out hover:bg-gray-200 hover:text-gray-800"
+            className="text-gray-600 dark:text-gray-400 font-bold px-10 h-screen text-4xl transition duration-300 ease-in-out hover:bg-gray-200 hover:dark:bg-[#222426] hover:text-gray-100"
           >
             <GrPrevious />
           </button>
         </div>
 
         <div className="fixed right-4 z-10 flex items-center">
-          <button 
+          <button
             onClick={navigateToNext}
-            className="text-gray-600 dark:text-gray-200 font-bold px-10 h-screen text-4xl transition duration-300 ease-in-out hover:bg-gray-200 hover:text-gray-800"
+            className="text-gray-600 dark:text-gray-400 font-bold px-10 h-screen text-4xl transition duration-300 ease-in-out hover:bg-gray-200 hover:dark:bg-[#222426] hover:text-gray-100"
           >
             <GrNext />
           </button>
